b8b83fce03ac09431015dde4e053b9df1cbac8c8
tdubs <contact-tdubs@protonmail.com>
2021-09-02 11:03:35 -0700

    added React app

--- api/server.js
+++ api/server.js
@@ -3,13 +3,15 @@ const cors = require('cors')
 const helmet = require('helmet')
 const {logger} = require('./middleware/middleware')
 const router = require('./router')
+const path = require('path')
+
 const server = express()
 
 server.use(express.json())
 server.use(cors())
 server.use(helmet())
-server.use('/api/', logger, router)
+server.use('/api', logger, router)
 
-server.get('*', (req, res) => res.send(`<h2>Remember your why</h2>`))
+server.get('*', (req, res) => res.sendFile(path.join(__dirname, 'client/client/build', 'index.html')))
 
 module.exports = server
